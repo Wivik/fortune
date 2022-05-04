a347b456c3f5a9745b2ccf6a117e05089aaaffd4
Wivik <48727868+Wivik@users.noreply.github.com>
2022-05-04 22:12:49 +0200

    fix: script realpath

--- scripts/fortune.sh
+++ scripts/fortune.sh
@@ -3,19 +3,20 @@
 ## run fortune, store the message, and cowsay it in various way
 
 message=$(mktemp)
+mypath=$(dirname $0)
 
 fortune > $message
 
-cat $message | cowsay > cowsay.txt
-cat $message | cowsay -f tux > tuxsay.txt
+cat $message | cowsay > ${mypath}/cowsay.txt
+cat $message | cowsay -f tux > ${mypath}/tuxsay.txt
 
 rm $message
 
 ## execute the python script
 
-/usr/bin/env python3 fortune.py
+/usr/bin/env python3 ${mypath}/fortune.py
 
 ## cleanup the mess
 
-rm cowsay.txt
-rm tuxsay.txt
+rm ${mypath}/cowsay.txt
+rm ${mypath}/tuxsay.txt
