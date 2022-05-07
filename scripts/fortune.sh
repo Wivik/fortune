9bdefe10ed24d48c0443da539b6a8d36766d0da8
Wivik <48727868+Wivik@users.noreply.github.com>
2022-05-07 09:33:46 +0200

    cleanup: removed histo from shell

--- scripts/fortune.sh
+++ scripts/fortune.sh
@@ -6,16 +6,6 @@ message=$(mktemp)
 now=$(date +"%s")
 mypath=$(dirname $0)
 
-## Create the history path if missing
-
-if [ ! -d "${mypath}/histo" ]; then
-    mkdir ${mypath}/histo
-fi
-
-## Purge older messages than ten days
-
-find ${mypath}/histo -type f -mtime +10
-
 ## generate today's fortune
 
 fortune > $message
@@ -24,11 +14,6 @@ cat $message | cowsay > ${mypath}/cowsay.txt
 cat $message | cowsay -f tux > ${mypath}/tuxsay.txt
 
 rm $message
-
-## Copy them to the histo folder for the rss feed
-
-cp ${mypath}/cowsay.txt ${mypath}/histo/cowsay_${now}.txt
-cp ${mypath}/tuxsay.txt ${mypath}/histo/tuxsay_${now}.txt
 
 ## execute the python script to generate the web pages and the rss feed
 
