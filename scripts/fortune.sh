e59b2d5630327b27eddcbe74554346342ead6beb
Wivik <48727868+Wivik@users.noreply.github.com>
2022-05-07 23:52:36 +0200

    feat: a different cowfile according to specific dates

--- scripts/fortune.sh
+++ scripts/fortune.sh
@@ -3,16 +3,41 @@
 ## run fortune, store the message, and cowsay it in various way
 
 message=$(mktemp)
-now=$(date +"%s")
 mypath=$(dirname $0)
-
+day=$(date +"%d")
+month=$(date +"%m")
+# day="20"
+# month="09"
 ## generate today's fortune
 
 fortune > $message
 
-cat $message | cowsay > ${mypath}/cowsay.txt
-cat $message | cowsay -f tux > ${mypath}/tuxsay.txt
+## we adapt the cowsay file according to a date
+case $day$month in
+    "0405") cat $message | cowsay -f vader > ${mypath}/cowsay.txt; cat $message | cowsay -f vader > ${mypath}/tuxsay.txt ;;
+    "2305") cat $message | cowsay -f turtle > ${mypath}/cowsay.txt; cat $message | cowsay -f turtle > ${mypath}/tuxsay.txt ;;
+    "0106") cat $message | cowsay -f supermilker > ${mypath}/cowsay.txt; cat $message | cowsay -f supermilker > ${mypath}/tuxsay.txt ;;
+    "0607") cat $message | cowsay -f kiss > ${mypath}/cowsay.txt; cat $message | cowsay -f kiss > ${mypath}/tuxsay.txt ;;
+    "0808") cat $message | cowsay -f meow > ${mypath}/cowsay.txt; cat $message | cowsay -f meow > ${mypath}/tuxsay.txt ;;
+    "1008") cat $message | cowsay -f moofasa > ${mypath}/cowsay.txt; cat $message | cowsay -f moofasa > ${mypath}/tuxsay.txt ;;
+    "2009") cat $message | cowsay -f head-in > ${mypath}/cowsay.txt; cat $message | cowsay -f head-in > ${mypath}/tuxsay.txt ;;
+    "3110") cat $message | cowsay -f skeleton > ${mypath}/cowsay.txt; cat $message | cowsay -f skeleton > ${mypath}/tuxsay.txt ;;
+    *) cat $message | cowsay > ${mypath}/cowsay.txt; cat $message | cowsay -f tux > ${mypath}/tuxsay.txt ;;
+esac
 
+# if [ "$day$month" = "0405" ]; then
+#     cat $message | cowsay -f vader > ${mypath}/cowsay.txt
+#     cat $message | cowsay -f vader > ${mypath}/tuxsay.txt
+# elif [ "$day$month" = "0808" ]; then
+#     cat $message | cowsay -f skeleton > ${mypath}/cowsay.txt
+#     cat $message | cowsay -f skeleton > ${mypath}/tuxsay.txt
+# elif [ "$day$month" = "31" ] && [ "$month" = "10" ]; then
+#     cat $message | cowsay -f skeleton > ${mypath}/cowsay.txt
+#     cat $message | cowsay -f skeleton > ${mypath}/tuxsay.txt
+# else
+#     cat $message | cowsay > ${mypath}/cowsay.txt
+#     cat $message | cowsay -f tux > ${mypath}/tuxsay.txt
+# fi
 rm $message
 
 ## execute the python script to generate the web pages and the rss feed
